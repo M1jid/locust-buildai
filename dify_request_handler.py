141576d2d517aad15f74673c0b14e162c886c0ea
ErfanVahedi <erfan.miaad@gmail.com>
2024-03-18 15:09:32 +0330

    add stuff

---
+++ dify_request_handler.py
@@ -0,0 +1,29 @@
+from SDK import base
+
+from . import settings
+
+
+class DifyRequestHandler(BaseHTTPSDK):
+    PASSPORT_URL = settings.PASSPORT_API_URL + "passport"
+    CHAT_MESSAGES_URL = settings.CHAT_MESSAGES_API_URL + "chat-messages"
+
+    def __init__(self):
+        pass
+
+    def get_token(self, app_code):
+        response = self.post(self.PASSPORT_URL, headers={"X-App-Code": app_code})
+        return response
+
+    def create_message(self, message, token):
+        data = {
+            message: message
+        }
+        headers = {
+            "Authorization": f"barear {token}"
+        }
+        response = self.post(
+            self.CHAT_MESSAGES_URL,
+            data=data,
+            headers=headers
+        )
+        return response
--- request_handler.py
+++
@@ -1,6 +0,0 @@
-from SDK import  base
-class DifyRequestHandler:
-    def __init__(self,)
-        pass
-    def authenticate(self):
-        BaseHTT
